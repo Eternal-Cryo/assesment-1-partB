3686e15345a51f03726567c8b2b8d37cc795b701
Joss <jdsard10@gmail.com>
2026-02-20 14:34:06 +1030

    linear search done
    the code to search an array through checking each number has been done

--- assesmetPartB/6)linearSearch/6)linearSearch.cpp
+++ assesmetPartB/6)linearSearch/6)linearSearch.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+
 //You will write a function that performs a linear search through an integer array by passing the target value, and the array, as function arguments.The function will return the index where the target is found, otherwise it will return -1.
 //Commit this to version control.
 //
@@ -19,8 +21,86 @@ using namespace std;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// a function that allows me search any array that is given
+int search(int array[], int array_size, int search_value)
+{
+
+	//a loop to search through the array for each index value 
+	for (int i = 0; i < array_size; i++)
+	{
+		//cout << array[i] << endl;
+		//asking the index if the search value and and number at the index is the same 
+		if (search_value == array[i])
+		{
+			//return the index value 
+			return i;
+
+
+		}
+
+	}
+	// if it isnt on the list it gives a -1 
+	return -1;
+
+};
+
+
+
 
 int main()
 {
-   
+	//storing the grabbed a word for the while loop
+	string exit = "";
+
+	//storing the grabbed a number from the player to search through the array for 
+	int user_number = 0;
+
+	//the array we are searching through 
+	int search_array[] = { 67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29 };
+
+
+	//simple while loop to continue the search or to exit the program
+	while (exit != "exit")
+	{
+
+
+
+
+
+		//asking and grabbing the number from the player: note it doesn't like letters or punctuation
+		cout << "please enter a number to search the array" << endl;
+		cin >> user_number;
+
+
+
+
+		//finding the index//  imputting the variable for the search
+		//   |						\/	then the user value
+		//  \/                                   \/
+		int result = search(search_array, 20, user_number);
+
+		// reading out the index value or a -1 if not in the array
+		cout << result << endl;
+
+		// checking if its in the array 
+		if (result >= 0) {
+			// if it is then say the number                                 then the index it was found at
+			//        \/													\/
+			cout << user_number << " number was found at index number " << result << endl;
+		}
+		else
+		{	//if its a negative print that it couldnt be found
+			cout << user_number << " could not be found" << endl;
+
+		}
+
+
+		//doesnt care what is typed unless it is exit
+		cout << "go again or exit" << endl;
+		cin >> exit;
+	}
+
+	//end of code stuff
+	cout << "thanks for playing!" << endl;
+	cout << "---------- end of code ----------" << endl;
 }
