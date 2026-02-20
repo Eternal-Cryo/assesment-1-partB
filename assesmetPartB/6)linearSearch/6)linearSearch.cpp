9b32a7523330dd749e02d91d48ff7765dc8808d6
Joss <jdsard10@gmail.com>
2026-02-20 14:26:43 +1030

    update
    adding task 6, 7/8 and 9

---
+++ assesmetPartB/6)linearSearch/6)linearSearch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//You will write a function that performs a linear search through an integer array by passing the target value, and the array, as function arguments.The function will return the index where the target is found, otherwise it will return -1.
+//Commit this to version control.
+//
+//Add an int main() function.Initialise this array
+//
+//67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29
+//
+//Add four asserts to check your function works.
+//3 should return 2
+//7 should return 11
+//67 should return 0
+//88 should return -1
+//Ensure all asserts() pass when you build and run.
+//Test and commit this to version control.
+//Write a loop that asks the user for a value and then calls your function to search for that value in the following array :
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+int main()
+{
+   
+}
