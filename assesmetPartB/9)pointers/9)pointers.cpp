416dcdac79e7351775013c136b107df7539a28d5
Joss <jdsard10@gmail.com>
2026-02-27 15:38:02 +1030

    update 9)pointers
    writing the initial code

--- assesmetPartB/9)pointers/9)pointers.cpp
+++ assesmetPartB/9)pointers/9)pointers.cpp
@@ -2,19 +2,58 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+
+//You will write a function to swap the value of two integers.You will do this by passing pointers to those integers into the function and performing the swap through pointer manipulation.Your function will not return any values.
+//Do NOT use std::swap.
+//Commit this to version control.
+//Add an int main().Initialise two integers to different values.Use your function to swap them in place.Add an assert()to confirm the values have been swapped.
+//Ensure all asserts() pass when you build and run.
+//Test and commit this to version control.
+//
+//
+//
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string exit = "";
+    int number01 = 0;
+    int number02 = 0;
+    int tempnumber = 0;
+
+
+    int *pnumber01 = &number01;
+    int *pnumber02 = &number02;
+    int *ptempnumber = &tempnumber;
+
+
+
+    
+    cout << "pick a number and ill swap them using pointers " << endl;
+    cin >> number01;
+    cout << *pnumber01 << endl;
+
+    cout << "now pick another number and ill swap them using pointers " << endl;
+    cin >> number02;
+    cout << *pnumber02 << endl;
+
+    *ptempnumber = *pnumber01;
+    *pnumber01 = *pnumber02;
+    *pnumber02 = *ptempnumber;
+
+
+
+    cout << " the first number is now " << *pnumber01 << endl;
+
+    cout << " the second number is now " << *pnumber02 << endl;
+
+
+
+    cout << "----------- end of code --------------" << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
