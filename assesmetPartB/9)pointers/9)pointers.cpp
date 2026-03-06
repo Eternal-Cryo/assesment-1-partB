fd701673bfad8910bd8664074fe376a3aef4cb65
Joss <jdsard10@gmail.com>
2026-03-06 15:53:44 +1030

    update 9) pointers
    added assert checks

--- assesmetPartB/9)pointers/9)pointers.cpp
+++ assesmetPartB/9)pointers/9)pointers.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include <cassert>
+
 using namespace std;
 
 
@@ -19,35 +21,50 @@ using namespace std;
 //
 //
 
-void swap(int number01, int number02) 
-{
+ 
+        
+
+
+// its a bool due to it returning a true or fals comand
+bool swap(int number01, int number02) 
+{   
+    // temp
      int tempNumber = 0;
 
-
+     // assigning the pointers to the original variables
     int *pNumber01 = &number01;
     int *pNumber02 = &number02;
     int *pTempNumber = &tempNumber;
-   
+   // reading out the preswapped pointers
    cout << *pNumber01 << endl;
    cout << *pNumber02 << endl;
-    
+    // the pointers get swapped using a temp number as a place holder
     *pTempNumber = *pNumber01;
     *pNumber01 = *pNumber02;
     *pNumber02 = *pTempNumber;
 
-
-
+    // reads out the changed numbers 
     std::cout << " the first number is now " << *pNumber01 << endl;
-
     std::cout << " the second number is now " << *pNumber02 << endl;
 
+    // runs the check to see if 1 swapped places with 2 if it isnt 1 it'll stop working due to the assert check at the end but will skip over this
+     if ((*pNumber01 == 2) && (*pNumber02 == 1))
+    {//derefrences the pointers then returns true
+         pNumber01 = nullptr;
+         pNumber02 = nullptr;
+         pTempNumber = nullptr;
+        return true;
+
+
+    }
+        
     // derefrencing the pointers
     pNumber01 = nullptr;
     pNumber02 = nullptr;
     pTempNumber = nullptr;
-
+    // if it isnt true its false
+    return false;
 }
-
 
 
 
@@ -57,11 +74,18 @@ int main()
 {
     string exit = "";
    
-     int number01 = 0;
-    int number02 = 0;
-   
-    
-    while (exit != "exit")
+     int number01 = 1;
+    int number02 = 2;
+    bool confirmed = false;
+
+    // the swapping 
+    confirmed = swap(number01, number02);
+    //the assert check seeing if they swapped 
+     assert(confirmed == true);
+
+
+     //a  while script for random numbers 
+    /*while (exit != "exit")
     {
 
         std::cout << "pick a number and ill swap them using pointers " << endl;
@@ -76,7 +100,7 @@ int main()
 
         std::cout << "want to go again" << endl;
         std::cin >> exit;
-    }
+    }*/
      
 
     std::cout << "----------- end of code --------------" << endl;
